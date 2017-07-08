8aae3cecb7ff0037ca892b9354629685561abdc3
Woonsan Ko <woonsan.ko@bloomreach.com>
2017-07-07 21:21:33 -0400

    Adding new demo

---
+++ demo/site/src/main/java/org/onehippo/forge/folderctxmenus/demo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.onehippo.forge.folderctxmenus.demo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="folderctxmenusdemo:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
